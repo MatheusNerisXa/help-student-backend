ea8f9d68a5b50a89ab2728ef3891b6645cb0cc2e
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-06-02 19:54:37 -0300

    modulo de login

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dtos/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  async login(LoginDto: LoginDto): Promise<UserEntity> {
+    const user: UserEntity | undefined = await this.userService
+      .findUserByEmail(LoginDto.email)
+      .catch(() => undefined);
+
+    const isMatch = await compare(LoginDto.password, user?.password || '');
+
+    if (!user || !isMatch) {
+      throw new NotFoundException('Email or password invalid');
+    }
+    return user;
+  }
+}
