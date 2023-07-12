c56e4fd32d63887be021f57394b0074ce4059474
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-07-11 21:44:30 -0300

    criando migration de materias

---
+++ src/migration/1689121836394-create-table-discipline.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableDiscipline1689121836394 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+          CREATE TABLE discipline (
+              id SERIAL PRIMARY KEY NOT NULL,
+              name VARCHAR(255) NOT NULL,
+              user_id INT,
+              status_discipline_id INT,
+              created_at TIMESTAMP DEFAULT now(),
+              updated_at TIMESTAMP DEFAULT now(),
+              FOREIGN KEY (user_id) REFERENCES public.user(id),
+              FOREIGN KEY (status_discipline_id) REFERENCES public.discipline_status(id)
+          );
+       `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+          DROP TABLE discipline;
+       `);
+  }
+}
